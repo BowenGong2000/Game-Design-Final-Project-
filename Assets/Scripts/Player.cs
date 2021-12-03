4d7871d96e62e0174244c899645c4cf1b7f5d0d9
Bowen Gong <67953712+BowenGong2000@users.noreply.github.com>
2021-12-02 23:36:07 -0500

    Add Animation
    -add three animation standing, walking, jumping
    -small problem with jumping, the code of jumping animation is added and commented out

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,13 +8,17 @@ public class Player : MonoBehaviour
 {
     public JoystickMovement joystickMovement;
     public float movespeed;
+    public bool facingLeft = true;
     private Rigidbody2D rb;
+
     //jump
     public Transform feet;
     public bool grounded = false;
     public LayerMask groundLayer;
     public Vector2 MoveDir;
 
+    //Animations
+    public Animator playerAnimator;
 
     void Start()
     {
@@ -29,6 +33,8 @@ public class Player : MonoBehaviour
             rb.AddForce(Vector2.up*5000f);
             PublicVars.Jump = true;
             rb.gravityScale = 15f;
+            // enable the jump animation
+            // playerAnimator.SetBool("IsJumping", true);
         }
 
         if (grounded)
@@ -36,24 +42,51 @@ public class Player : MonoBehaviour
             PublicVars.Jump = false;
             PublicVars.doubleJump = false;
             rb.gravityScale = 15f;
+            // disable the jump animation
+            // playerAnimator.SetBool("IsJumping", false);
         }
          if (CrossPlatformInputManager.GetButtonDown("Jump") && PublicVars.Jump && PublicVars.ableToDoubleJump && !PublicVars.doubleJump)
         {
             rb.AddForce(Vector2.up*5000f);
             PublicVars.doubleJump = true;
             rb.gravityScale = 30f;
+            // enable the jump animation
+            // playerAnimator.SetBool("IsJumping", true);
         }
     }
     void FixedUpdate()
-    {
-        if (joystickMovement.joystickVec.x != 0) // move when detecting joystick movement in x direction
+
+    {   // move when detecting joystick movement in x direction
+        if (joystickMovement.joystickVec.x != 0) 
         {
             rb.velocity = new Vector2(joystickMovement.joystickVec.x * movespeed, 0);
+            //activate the walking animation 
+            playerAnimator.SetFloat("PlayerSpeed", Mathf.Abs(joystickMovement.joystickVec.x));
         }
         else // remain still
         {
             rb.velocity = Vector2.zero;
+            // deactivate walking animation
+            playerAnimator.SetFloat("PlayerSpeed", 0);
         }
+
+        // check facing direction
+        if (joystickMovement.joystickVec.x>0&& !facingLeft)
+        {
+            FlipDirection();
+        }
+        else if (joystickMovement.joystickVec.x<0 && facingLeft)
+        {
+            FlipDirection();
+        }
+    }
+
+    private void FlipDirection()
+    {
+        facingLeft = !facingLeft;
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
     }
 
     void OnTriggerEnter2D(Collider2D other)
