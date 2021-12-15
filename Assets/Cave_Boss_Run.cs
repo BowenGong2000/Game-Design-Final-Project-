2d5ae9d65b7fab338d09424248a0545ef90663bc
Bowen Gong <67953712+BowenGong2000@users.noreply.github.com>
2021-12-15 13:27:50 -0500

    Update Cave_Boss_Run.cs

--- Assets/Cave_Boss_Run.cs
+++ Assets/Cave_Boss_Run.cs
@@ -25,7 +25,7 @@ public class Cave_Boss_Run : StateMachineBehaviour
         boss.LookAtPlayer();
 
         Vector2 target = new Vector2(player.position.x, rb.position.y);
-        Vector2 newPos = Vector2.MoveTowards(rb.position, target, speed );
+        Vector2 newPos = Vector2.MoveTowards(rb.position, target, speed * Time.fixedDeltaTime);
         rb.MovePosition(newPos);
 
         if (Vector2.Distance(player.position, rb.position) <= attackRange)
