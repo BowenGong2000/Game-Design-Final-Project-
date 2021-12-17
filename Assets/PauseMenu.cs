d0e65f653db5893cad3f715f98a1ab3fcd08f678
Bowen Gong <67953712+BowenGong2000@users.noreply.github.com>
2021-12-16 22:54:23 -0500

    bug fix

--- Assets/PauseMenu.cs
+++ Assets/PauseMenu.cs
@@ -11,7 +11,7 @@ public class PauseMenu : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown("Pause"))
+        if (Input.GetButtonDown("Pause"))
         {
             if (GameIsPaused)
             {
